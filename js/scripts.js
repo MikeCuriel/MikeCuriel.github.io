28364c584c53c53312fb9aaf0e1f2e0937449e36
MikeCuriel <m.curiel0611@gmail.com>
2024-03-15 01:42:47 -0600

    Ventana inicial bloqueada
    Ventana inicial bloqueada

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,24 @@
 window.onload = function() {
-    var modal = document.getElementById("miModal");
-    var span = document.getElementsByClassName("cerrar")[0];
-    var audio = document.getElementById("miMusica");
-  
-    // Abrir la modal automáticamente
-    modal.style.display = "block";
-  
-    // Cuando el usuario hace click en <span> (x), cierra la modal
-    span.onclick = function() {
+  var modal = document.getElementById("miModal");
+  var span = document.getElementsByClassName("cerrar")[0];
+  var audio = document.getElementById("miMusica");
+
+  // Abrir la modal automáticamente
+  modal.style.display = "block";
+  document.body.style.overflow = "hidden"; 
+
+  // Cuando el usuario hace click en <span> (x), cierra la modal
+  span.onclick = function() {
+    modal.style.display = "none";
+    document.body.style.overflow = "auto"; 
+    audio.play(); // Reproduce la música después de cerrar la modal
+  }
+
+  // También cierra la modal y reproduce la música si se hace clic fuera de ella
+  window.onclick = function(event) {
+    if (event.target == modal) {
       modal.style.display = "none";
-      audio.play(); // Reproduce la música después de cerrar la modal
-    }
-  
-    // También cierra la modal y reproduce la música si se hace clic fuera de ella
-    window.onclick = function(event) {
-      if (event.target == modal) {
-        modal.style.display = "none";
-        audio.play();
-      }
+      audio.play();
     }
   }
+}
