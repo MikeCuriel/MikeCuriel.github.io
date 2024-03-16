798c634b7f0f51f2e8bb3cfedc397f87de64e184
MikeCuriel <m.curiel0611@gmail.com>
2024-03-16 13:45:53 -0600

    Tiempo online
    Agregar el tiempo para ver como se ve en dispositivos moviles

--- js/scripts.js
+++ js/scripts.js
@@ -46,3 +46,32 @@ $(document).ready(function(){
   });
 });
 
+ // Establecer la fecha de finalización del contador
+ var countdownDate = new Date("Apr 27, 2024 16:00:00").getTime();
+
+ // Actualizar el contador cada segundo
+ var countdown = setInterval(function() {
+   // Obtener la fecha y hora actuales
+   var now = new Date().getTime();
+   
+   // Calcular la diferencia entre la fecha de finalización y la fecha actual
+   var distance = countdownDate - now;
+   
+   // Calcular los días, horas, minutos y segundos restantes
+   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+   
+  // Mostrar el resultado en los elementos HTML
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+   
+   // Si el contador llega a cero, detenerlo
+   if (distance < 0) {
+     clearInterval(countdown);
+     document.getElementById("countdown").innerHTML = "EXPIRED";
+   }
+ }, 1000);
