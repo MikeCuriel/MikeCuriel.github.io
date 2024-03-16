cd6d77917ebc44927d846e47ebf9fd119fdfbd4e
MikeCuriel <m.curiel0611@gmail.com>
2024-03-15 22:29:26 -0600

    Whats
    Verificar si manda mensaje al numero proporciionado

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,8 @@ window.onload = function() {
   var modal = document.getElementById("miModal");
   var span = document.getElementsByClassName("cerrar")[0];
   var audio = document.getElementById("miMusica");
+  var stop = document.getElementById("btnMusic");
+
 
   // Abrir la modal automáticamente
   modal.style.display = "block";
@@ -21,4 +23,26 @@ window.onload = function() {
       audio.play();
     }
   }
+
+  stop.onclick = function(event) {
+    if (event.target == stop) {
+      if(!audio.paused){
+        modal.style.display = "none";
+        audio.pause();
+      }
+      else{
+        audio.play();
+      }
+    }
+  }
 }
+
+$(document).ready(function(){
+  $("#btnWhats").click(function(){
+      var phoneNumber = "+5213319923518"; // Reemplaza con el número de teléfono al que deseas enviar el mensaje
+      var message = "La familia Bibiana Morales confirma asistencia" ; // Mensaje a enviar
+      var url = "https://wa.me/" + phoneNumber + "?text=" + encodeURIComponent(message);
+      window.open(url, "_blank");
+  });
+});
+
