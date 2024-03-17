d64dcc2b4365ece781ea679695f178e7ed7e2a12
MikeCuriel <m.curiel0611@gmail.com>
2024-03-17 00:09:13 -0600

    Solucion del error de envio de mensaje whats

--- js/indexScript.js
+++ js/indexScript.js
@@ -1,31 +1,19 @@
 $(document).ready(function(){
-    $("#btnWhats").click(function(){
-        var phoneNumber = "+5213319923518"; // Reemplaza con el número de teléfono al que deseas enviar el mensaje
-        var message = $(this).data("message");
-        var url = "https://wa.me/" + phoneNumber + "?text=" + encodeURIComponent(message);
+    $(".EnviarInvitacion").click(function(){
+        // Obtener el valor del primer <td> (nombre)
+        var primerTD = $(this).closest('tr').find('td:first-child').text();
+        
+        // Obtener el nombre de la persona desde el atributo data-nombre
+        var nombre = $(this).attr('data-nombre');
+  
+        // Obtener el número de teléfono de la misma fila
+        var telefono = $(this).closest('tr').find('.NombreContacto').val();
+
+        // Crear el enlace de WhatsApp con el nombre y el número de teléfono
+        var mensaje = encodeURIComponent(primerTD + ' te invito a mi fiesta de XV de Jatziry Giselle. Haz clic aquí para unirte: https://mikecuriel.github.io/pagina/Familia/' + nombre.replace(" ", "") + '.html');
+        var url = "https://wa.me/" + "+521" + telefono + "?text=" + mensaje;
+
+        // Abrir el enlace en una nueva pestaña
         window.open(url, "_blank");
     });
   });
-
-// $(document).ready(function(){
-//     $("#EnviarInvitacion").click(function(){
-  
-//         // Obtener el nombre de la persona desde el atributo data-nombre
-//         var nombre = $(this).attr('data-nombre');
-  
-//         // Obtener el número de teléfono de la misma fila
-//         var telefono = $(this).closest('tr').find('.NombreContacto').val();
-  
-//         var phoneNumber = "+5213319923518";
-//         var message = "Hola";
-//         alert("Nombre obtenido: " + phoneNumber);
-//         // Crear el enlace de WhatsApp con el nombre y el número de teléfono
-//         var mensaje = encodeURIComponent(nombre + ' te invito a mi fiesta de XV de Jatziry Giselle. Haz clic aquí para unirte: https://mikecuriel.github.io/pagina/Familia/' + nombre.replace(" ", "") + '.html');
-//         var url = "https://wa.me/" + phoneNumber + "?text=" + encodeURIComponent(message);
-  
-//         alert("URL:" + url);
-
-//         // Abrir el enlace en una nueva pestaña
-//         window.open(url, "_blank");
-//     });
-//   });
